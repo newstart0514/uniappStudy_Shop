1c3d9ea936a4840adc2f1caa7e1181c1cb962786
lbg <1418591636@qq.com>
2023-01-16 19:54:46 +0800

    首页功能完成

--- main.js
+++ main.js
@@ -3,6 +3,29 @@
 import Vue from 'vue'
 import App from './App'
 
+// 导入网络请求的包
+import { $http } from '@escook/request-miniprogram'
+uni.$http = $http
+$http.beforeRequest = function() {
+	uni.showLoading({
+		title: '数据加载中...'
+	})
+}
+$http.afterRequest = function(options) {
+	uni.hideLoading()
+}
+// 请求的根路径
+$http.baseUrl = 'https://api-hmugo-web.itheima.net'
+
+// 封装一个错误消息模板  带有默认值
+uni.$showMsg = function (title = '数据加载失败！', duration = 1500) {
+	uni.showToast({
+		title,
+		duration,
+		icon:'none'
+	})
+}
+
 Vue.config.productionTip = false
 
 App.mpType = 'app'
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,6 +1,53 @@
 <template>
 	<view>
-		home
+		<!-- 轮播图区域 -->
+		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
+			<!-- 循环渲染轮播图的item项 -->
+			<swiper-item v-for="(item, i) in swiperList" :key="i" class="swiper-item">
+				<!-- 展示图片 -->
+				<navigator :url="`/subpkg/goods_details/goods_details?goods_id=${item.goods_id}`">
+					<!-- 动态绑定图片的src -->
+					<image :src="item.image_src"></image>
+				</navigator>
+			</swiper-item>
+		</swiper>
+		<!-- 分类导航区域 -->
+		<view class="navBox">
+			<view v-for="(item, i) in navList" :key="i" @click="navClickHandler(item)">
+				<image :src="item.image_src"></image>
+			</view>
+		</view>
+		<!-- 楼层区域 -->
+		<view class="floor">
+			<view class="floor-item" v-for="(item, i) in floorList" :key="i">
+				<!-- 楼层标题 -->
+				<image :src="item.floor_title.image_src" class="floor_title"></image>
+				<!-- 楼层图片盒子 -->
+				<view class="floorImgBox">
+					<!-- 左侧的大盒子 -->
+					<navigator class="leftImgBox" :url="item.product_list[0].url">
+						<image 
+							:src="item.product_list[0].image_src" 
+							:style="{width: item.product_list[0].image_width + 'rpx'}" 
+							mode="widthFix"
+						>
+						</image>
+					</navigator>
+					<!-- 右侧的四个小盒子 -->
+					<view class="rightImgBox">
+						<navigator 
+							class="rightImgBoxItem" 
+							v-for="(item2, i2) in item.product_list" 
+							:key="i2" 
+							v-if="i2 !== 0" 
+							:url="item2.url"
+						>
+							<image :src="item2.image_src" :style="{width: item2.image_width + 'rpx'}" mode="widthFix"></image>
+						</navigator>
+					</view>
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,12 +55,106 @@
 	export default {
 		data() {
 			return {
-				
+				// 这是轮播图的数据列表
+				swiperList: [],
+				// 分类导航数据列表
+				navList: [],
+				// 楼层数据列表
+				floorList: []
 			};
+		},
+		onLoad() {
+			// 调用方法，获取轮播图的数据
+			this.getSwiperList()
+			// 调用方法获取分类导航的数据
+			this.getNavList()
+			// 调用方法获取楼层数据
+			this.getFloorList()
+		},
+		methods: {
+			// 获取轮播图数据的方法
+			async getSwiperList() {
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/home/swiperdata')
+				// 请求失败
+				if (res.meta.status !== 200) return uni.showMsg()
+				// 请求成功将数据转移到data
+				this.swiperList = res.message
+			},
+			// 获取分类导航的数据的方法
+			async getNavList() {
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/home/catitems')
+				// 请求失败
+				if (res.meta.status !== 200) return uni.$showMsg()
+				// 请求成功，将数据存入data
+				this.navList = res.message
+			},
+			// 获取楼层区域的数据的方法
+			async getFloorList() {
+				const {
+					data: res
+				} = await uni.$http.get('/api/public/v1/home/floordata')
+				// 请求失败
+				if (res.meta.status !== 200) return uni.$showMsg()
+				// 请求成功，将数据存入data
+				// 通过双层的循环，处理url地址
+				res.message.forEach(floor => {
+					floor.product_list.forEach(prod => {
+						prod.url = '/subpkg/goods_list/goods_list?' + prod.navigator_url.split('?')[1]
+					})
+				})
+				this.floorList = res.message
+			},
+			// 导航栏上的图标的点击事件
+			navClickHandler(item) {
+				if (item.name === '分类') {
+					uni.switchTab({
+						url: '/pages/cate/cate'
+					})
+				}
+			}
 		}
 	}
 </script>
 
 <style lang="scss">
+	// 轮播图
+	swiper {
+		height: 330rpx;
 
+		.swiper-item,
+		image {
+			width: 100%;
+			height: 330rpx;
+		}
+	}
+	// 导航
+	.navBox {
+		display: flex;
+		margin: 15px 0;
+		justify-content: space-around;
+
+		image {
+			width: 128rpx;
+			height: 140rpx;
+		}
+	}
+	// 楼层
+	.floor_title {
+		width: 100%;
+		height: 60rpx;
+		display: flex;
+	}
+	.rightImgBox {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-around;
+	}
+	.floorImgBox {
+		display: flex;
+		padding-left: 10rpx;
+	}
 </style>
